86ae993a1899cdc3d6cefe953504734e8fafc5e4
Hemanth B K <122110097+hanumantha628@users.noreply.github.com>
2023-08-22 12:11:19 +0530

    Delete python.py
    Signed-off-by: Hemanth B K  <122110097+hanumantha628@users.noreply.github.com>

--- python.py
+++
@@ -1,6 +0,0 @@
-import phonenumbers
-from phonenumbers import geocoder
-phone_number1 = phonenumbers.parse("+918296581864")
-
-print("\nPhone. Numbers Location\n")
-print(geocoder.description_for_number(phone_number1,"en"));
